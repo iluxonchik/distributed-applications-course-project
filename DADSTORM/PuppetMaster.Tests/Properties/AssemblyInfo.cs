0f2ac84ccebb05a6d795998a7c7122eae4bec946
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-10-07 09:48:15 +0100

    chore: set up project structure, set up NUnit

--- DADSTORM/PuppetMaster.Tests/Properties/AssemblyInfo.cs
+++ DADSTORM/PuppetMaster.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PuppetMaster")]
+[assembly: AssemblyTitle("PuppetMaster.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PuppetMaster")]
+[assembly: AssemblyProduct("PuppetMaster.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9470eb46-f205-4e09-8d4e-c940824c31f1")]
+[assembly: Guid("5f888d14-dc37-49bc-8b7b-c04150849a5d")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ DADSTORM/PuppetMaster.Tests/PuppetMasterConfigTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace PuppetMaster.Tests
+{
+    [TestFixture]
+    public class PuppetMasterConfigTest
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            // TODO
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // TODO
+        }
+
+        [Test]
+        public void TestTest()
+        {
+            // Should be removed once actual tests are written
+            Assert.That(true);
+        }
+
+    }
+}
