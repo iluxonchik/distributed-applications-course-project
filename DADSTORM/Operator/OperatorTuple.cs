fb76a37b288eaa57ac1f932370cbadb5d217719e
pauloanjos <paulo.15.gk@gmail.com>
2016-10-27 11:11:32 +0100

    Broken version with new tuple

---
+++ DADSTORM/Operator/OperatorTuple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class OperatorTuple
+    {
+        List<string> Tuple { get; set; }
+
+    public OperatorTuple (List<String> tuple)
+        {
+            this.Tuple = tuple;
+        }
+        
+    }
+}
