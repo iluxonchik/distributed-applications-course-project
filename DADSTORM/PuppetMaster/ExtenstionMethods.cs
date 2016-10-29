dccbd770cc381c8977f26e32ac1a33a5323cc3ae
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-10-29 23:08:34 +0100

    feat: add Get extension method to Dictionary
    Similiar to Dictionary.get() in Python.

---
+++ DADSTORM/PuppetMaster/ExtenstionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppetMaster
+{
+    public static class DictionaryExtensionMethods
+    {
+        /// <summary>
+        /// Pythonic "Dictionary.get()" method.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="def"></param> default value. Optional parameter, defaults to the default type of TValue
+        /// <returns></returns>
+        public static TValue Get<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue def = default(TValue))
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return dictionary[key];
+            } else
+            {
+                return def;
+            }
+        }
+    }
+}
