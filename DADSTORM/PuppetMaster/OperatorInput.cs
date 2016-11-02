bf11541f6945ab622eafa7947f97bbd542b39ef2
DMRodrigues <diogo.miguel.rodrigues@ist.utl.pt>
2016-11-02 15:47:04 +0000

    fix again

--- DADSTORM/PuppetMaster/OperatorInput.cs
+++ DADSTORM/PuppetMaster/OperatorInput.cs
@@ -9,19 +9,18 @@ namespace PuppetMaster
         public string Name;
         public List<string> Addresses { get; set; } = new List<string>(); // only applies to InputType.Operator
         public InputType Type { get; set; }
-    }
 
-    public override string ToString()
-    {
-        const string BASE_FORMAT = "Name: {0}, Type: {1}";
-        if (Type.Equals(InputType.File))
+        public override string ToString()
         {
-            return string.Format(BASE_FORMAT, Name, Type);
-        }
-        else
-        {
-            return string.Format(BASE_FORMAT + ", Address: {2}", Name, Type, Addresses);
+            const string BASE_FORMAT = "Name: {0}, Type: {1}";
+            if (Type.Equals(InputType.File))
+            {
+                return string.Format(BASE_FORMAT, Name, Type);
+            }
+            else
+            {
+                return string.Format(BASE_FORMAT + ", Address: {2}", Name, Type, Addresses);
+            }
         }
     }
-}
 }
